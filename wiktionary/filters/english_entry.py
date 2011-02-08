80c0c0920b297ee9efe825b4a8fb93c7240f717e
max99x <max99x@a5ebc66a-2334-11df-99a1-e5276f3be39f>
2011-02-08 19:07:16 +0000

    * Cleaned up and documented the English entry filter. No change in functionality. * Added an __init__.py to the filters folder to make it a package.

--- wiktionary/filters/english_entry.py
+++ wiktionary/filters/english_entry.py
@@ -1,47 +1,65 @@
+"""A filter for discarding entries that are not English definitions.
+
+Discards "special" entries and entries that do not contain English definitions.
+XML comments and all irrelevant XML elements are also discarded. The filter
+expects to be passed a raw dump.
+"""
+
+
 from base import WiktionaryFilter
 import re
 
+
+# An XML comment regex.
+RE_COMMENT = re.compile('<!--.*?-->', re.DOTALL)
+
+
 class EnglishEntryFilter(WiktionaryFilter):
-    '''
-    A filter for discarding "special" entries and entries that do not contain
-    English definitions. XML comments and all irrelevant XML elements are also
-    discarded. Accepts raw dump.
-    '''
+  """A filter for discarding entries that are not English definitions."""
 
-    RE_COMMENT = re.compile('<!--.*?-->', re.DOTALL)
-    
-    def __init__(self):
-        self._total = 0
-        self._accepted = 0
-        
-    def _endElement(self, name):
-        self._last_elem = None
-        if name not in ('title', 'text'): return
+  def __init__(self):
+    self._total = 0
+    self._accepted = 0
 
-        data = ''.join(self._buffer)
+  def _endElement(self, name):
+    """Writes accepted pages in a minimal form and discards the rest.
 
-        if name == 'title':
-            self._cur_title = data
-        elif name == 'text':
-            self._total += 1
-            if not self._cur_title:
-                raise Exception('No title for data: ' + data)
-            elif ':' in self._cur_title:
-                self._cur_title = None
-            else:
-                if '==English==' in data:
-                    data = self.RE_COMMENT.sub('', data)
-                    self._writePage(self._cur_title, data)
-                    self._accepted += 1
+    When a title element is encountered, it is recorded for future usage.
+    When a text element is encountered, it is checked for English definitions
+    and if found, written as a page with the current title.
+    All other elements are ignored (and therefore discarded).
 
-    def parseXmlDump(self, src_filename, dst_filename):
-        WiktionaryFilter.parseXmlDump(self, src_filename, dst_filename)
-        
-        return (self._total, self._accepted)
+    Args:
+      name: The name of the element which has just ended.
+    """
+    self._last_elem = None
+    if name not in ('title', 'text'): return
 
-if __name__ == '__main__':
-    filter = EnglishEntryFilter()
-    total, accepted = filter.parseXmlDump('enwiktionary-latest-pages-articles.xml',
-                                          'articles_out.xml')
-    print ('Retained %d entries from %d (%.2f%%).' %
-           (accepted, total, float(total) / accepted))
+    data = ''.join(self._buffer)
+
+    if name == 'title':
+      self._cur_title = data
+    elif name == 'text':
+      self._total += 1
+      if not self._cur_title:
+        raise Exception('No title for data: ' + data)
+      elif ':' in self._cur_title:
+        self._cur_title = None
+      else:
+        if '==English==' in data:
+          data = RE_COMMENT.sub('', data)
+          self._writePage(self._cur_title, data)
+          self._accepted += 1
+
+  def parseXmlDump(self, src_filename, dst_filename):
+    """Runs the filter.
+
+    Args:
+      src_filename: The filename of the source XML dump to be parsed.
+      dst_filename: The filename of the destination XML dump to be written.
+
+    Returns:
+      A 2-tuple of the total number of processed and accepted pages.
+    """
+    WiktionaryFilter.parseXmlDump(self, src_filename, dst_filename)
+    return (self._total, self._accepted)
