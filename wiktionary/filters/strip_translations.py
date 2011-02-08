21c6cf4268a4d98dc133167417d9c94bc5e5ac16
max99x <max99x@a5ebc66a-2334-11df-99a1-e5276f3be39f>
2011-02-08 19:41:41 +0000

    Cleaned up and documented the translation stripping filter. Functionality is unchanged.

--- wiktionary/filters/strip_translations.py
+++ wiktionary/filters/strip_translations.py
@@ -1,49 +1,50 @@
+"""Discards translation sections of Wiktionary pages.
+
+Expects input to be in the format produced by a WiktionaryFilter.
+"""
+
+
 from base import WiktionaryFilter
 import re
 
-class TraslationStrippingFilter(WiktionaryFilter):
-    '''
-    Discards the translation section of the articles. Expects input to be
-    in the format produced by a WiktionaryFilter.
-    '''
-    
-    RE_TRANSLATION_SECTION = re.compile(r'''
+
+RE_TRANSLATION_SECTION = re.compile(r'''
+(?:\r\n|\r|\n)
+={3,}Translations?={3,}
+(?:
     (?:\r\n|\r|\n)
-    ={3,}Translations?={3,}
+    .*?
     (?:
-        (?:\r\n|\r|\n)
-        .*?
-        (?:
-            (?=
-               (?:\r\n|\r|\n)
-               =
-            )
-          |
-            $
+        (?=
+           (?:\r\n|\r|\n)
+           =
         )
       |
         $
     )
-    ''', re.VERBOSE | re.DOTALL | re.IGNORECASE | re.UNICODE)
-
-    def _startElement(self, name, attrs):
-        if name == 'page':
-            self._cur_title = attrs['title']
-        self._buffer = []
-
-    def _endElement(self, name):
-        if name != 'page': return
-
-        data = ''.join(self._buffer)
-        data = self.RE_TRANSLATION_SECTION.sub('', data)
-        if '=Translations=' in data:
-            raise Exception(u'Found unstripped translation in %s: %s' %
-                            (self._cur_title, repr(data)))
-
-        self._writePage(self._cur_title, data)
+  |
+    $
+)
+''', re.VERBOSE | re.DOTALL | re.IGNORECASE | re.UNICODE)
 
 
-if __name__ == '__main__':
-    filter = TraslationStrippingFilter()
-    filter.parseXmlDump('articles_out2.xml', 'articles_out3.xml')
-    print 'Translation stripping filter successful.'
+class TraslationStrippingFilter(WiktionaryFilter):
+  """Discards translation sections of Wiktionary pages."""
+
+  def _startElement(self, name, attrs):
+    """Captures page titles."""
+    if name == 'page':
+      self._cur_title = attrs['title']
+    self._buffer = []
+
+  def _endElement(self, name):
+    """Removes translation sections and writes the page."""
+    if name != 'page': return
+
+    data = ''.join(self._buffer)
+    data = RE_TRANSLATION_SECTION.sub('', data)
+    if '=Translations=' in data:
+      raise Exception('Found unstripped translation in %s: %s' %
+                      (self._cur_title, repr(data)))
+
+    self._writePage(self._cur_title, data)
