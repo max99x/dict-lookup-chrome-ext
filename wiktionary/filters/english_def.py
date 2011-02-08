341e51de74eefbef571d21538f2f1c9fcbe8b497
max99x <max99x@a5ebc66a-2334-11df-99a1-e5276f3be39f>
2011-02-08 19:35:48 +0000

    Cleaned and documented the English def filter. Functionality unchanged.

--- wiktionary/filters/english_def.py
+++ wiktionary/filters/english_def.py
@@ -1,50 +1,51 @@
+"""Discards non-English sections of Wiktionary pages.
+
+Expects input to be in the format produced by a WiktionaryFilter.
+"""
+
+
 from base import WiktionaryFilter
 import re
 
-class EnglishDefinitionFilter(WiktionaryFilter):
-    '''
-    Discards non-English sections of entries containing both English and
-    non-English definitions. Expects input to be in the format produced by a
-    WiktionaryFilter.
-    '''
-    
-    RE_ENGLISH_SECTION = re.compile(r'''
-    (?:^|\r\n|\r|\n)
-    ==English==\s*
+
+RE_ENGLISH_SECTION = re.compile(r'''
+(?:^|\r\n|\r|\n)
+==English==\s*
+(?:\r\n|\r|\n)
+\s*
+(.*?)
+\s*
+(?:
     (?:\r\n|\r|\n)
-    \s*
-    (.*?)
-    \s*
-    (?:
-        (?:\r\n|\r|\n)
-        ----+
-        (?:\r\n|\r|\n)
-      |
-        (?:\r\n|\r|\n)
-        ==
-        \w
-      |
-        $
-    )
-    ''', re.VERBOSE | re.DOTALL | re.IGNORECASE | re.UNICODE)
+    ----+
+    (?:\r\n|\r|\n)
+  |
+    (?:\r\n|\r|\n)
+    ==
+    \w
+  |
+    $
+)
+''', re.VERBOSE | re.DOTALL | re.IGNORECASE | re.UNICODE)
 
-    def _startElement(self, name, attrs):
-        if name == 'page':
-            self._cur_title = attrs['title']
-        self._buffer = []
-    
-    def _endElement(self, name):
-        if name != 'page': return
 
-        data = ''.join(self._buffer)
-        english_sections = self.RE_ENGLISH_SECTION.findall(data)
-        if len(english_sections) != 1:
-            raise Exception(u'Expected 1 English section, not %d. Data: %s' %
-                            (len(english_sections), data))
-        
-        self._writePage(self._cur_title, english_sections[0])
+class EnglishDefinitionFilter(WiktionaryFilter):
+  """Discards non-English sections of multi-language Wiktionary pages."""
 
-if __name__ == '__main__':
-    filter = EnglishDefinitionFilter()
-    filter.parseXmlDump('articles_out.xml', 'articles_out2.xml')
-    print 'Definition filter successful.'
+  def _startElement(self, name, attrs):
+    """Captures page titles."""
+    if name == 'page':
+      self._cur_title = attrs['title']
+    self._buffer = []
+
+  def _endElement(self, name):
+    """Extracts and writes English sections from pages."""
+    if name != 'page': return
+
+    data = ''.join(self._buffer)
+    english_sections = RE_ENGLISH_SECTION.findall(data)
+    if len(english_sections) != 1:
+      raise Exception(u'Expected 1 English section, not %d. Data: %s' %
+                      (len(english_sections), data))
+
+    self._writePage(self._cur_title, english_sections[0])
